3bfe55c8f704e4683f2cdf78609380f56eadac4c
o0w0o-dev <o0w0o.dev@gmail.com>
2024-11-17 15:15:03 +0800

    feat: add forgot password button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Field from "../components/Field";
 
 export default function Login({ onLogin }) {
+  const navigate = useNavigate();
   return (
     <>
       <div className="login">
-        <div className="recovery-password-url">recovery password</div>
+        <div
+          className="recovery-password-url"
+          onClick={() => navigate("/forgot")}
+        >
+          recovery password
+        </div>
         <p className="don-t-have-an-account">
           Don't have an account yet? Sign Up
         </p>
