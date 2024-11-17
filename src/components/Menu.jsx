2d978be60489977f48482e932e315fd3d5a44809
o0w0o-dev <o0w0o.dev@gmail.com>
2024-11-17 10:24:31 +0800

    feat: update CSS calculation for menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,21 +2,23 @@ import { useNavigate } from "react-router-dom";
 
 export default function Menu({ menuStatus }) {
   const navigate = useNavigate();
+  const displayButton = menuStatus.filter((button) => button.display === true);
 
   return (
     <div className="menu">
-      {menuStatus
-        .filter((button) => button.display === true)
-        .map((button, index) => (
-          <div
-            className={`menu-button ${button.selected ? "selected" : "block"}`}
-            style={{ top: 296 - index * 74 }}
-            key={button.text}
-            onClick={() => navigate(button.path)}
-          >
-            <div className="text-24">{button.text}</div>
-          </div>
-        ))}
+      {displayButton.map((button, index) => (
+        <div
+          className={`menu-button ${button.selected ? "selected" : "block"}`}
+          style={{
+            top:
+              (menuStatus.length - displayButton.length - 2) * 74 + index * 74,
+          }}
+          key={button.text}
+          onClick={() => navigate(button.path)}
+        >
+          <div className="text-24">{button.text}</div>
+        </div>
+      ))}
     </div>
   );
 }
